febbc330ccc345e539c70ffe5aa951f22c72447f
danelbro <dan.elbro@gmail.com>
2024-10-04 00:00:32 +0100

    Add doxygen comment to Application.hpp

--- src/Application.hpp
+++ src/Application.hpp
@@ -6,6 +6,36 @@
 #include "SDL_Interface.hpp"
 #include "StageManager.hpp"
 
+/**
+ *
+ * Application is the base class from which your app should derive. When writing your
+ * derived app class, you are responsible for:
+ *
+ * - in your app’s constructor, adding at least one (derived) Stage to your app’s
+ *   StageManager by calling m_stageMan.add_stage(), and setting it as the current
+ *   stage by calling m_stageMan.set_current_stage(). Your derived stages will
+ *   contain all of your app’s logic and functionality.
+ *
+ * - setting the Renderer’s behaviour [currently, all you can do is
+ *   m_renderer.setVSync()].
+ *
+ * - overriding trigger_stage_change(). The StageManager calls this function when a
+ *   Stage returns a non-current Stage ID string from its update() or handle_input()
+ *   functions, passing in the new Stage ID string. In response to the new Stage ID,
+ *   in this function you should add a stage of that type to the StageManager by
+ *   calling m_stageMan.add_stage(). A switch statement is recommended! Make sure to
+ *   handle StageID::QUIT - you can call add_stage with a nullptr - and note that you
+ *   can put any behaviour you want on stage transitions here.
+ *
+ * - instantiating a derived Application class (use RAII!). You’ll need to decide on
+ *   at least a title and screen size (width and height), as well as any SDL window
+ *   flags you want.
+ *
+ * - in main(), calling yourApp.run() (remember to #include <SDL3/SDL_main.h>,
+ *   particularly if you’re on Windows).
+ *
+ */
+
 class Application
 {
 public:
