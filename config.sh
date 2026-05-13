229ffe2495d48ac7458af76b64ae9aa2d16ce566
danelbro <dan.elbro@gmail.com>
2026-05-13 12:33:23 +0100

    Add SDL_mixer submodule

--- config.sh
+++ config.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
 mkdir -p build/$1
-cd build/$1
-cmake -DCMAKE_BUILD_TYPE=$1 -DBUILD_SHARED_LIBS=OFF -DSDL_TEST_LIBRARY=OFF -DSDLTTF_VENDORED=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ../../
-cp compile_commands.json ../../
+cmake -S . -Bbuild/$1 -GNinja -DCMAKE_BUILD_TYPE=$1 -DBUILD_SHARED_LIBS=OFF -DSDL_TEST_LIBRARY=OFF -DSDLTTF_VENDORED=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+mv build/$1/compile_commands.json .
