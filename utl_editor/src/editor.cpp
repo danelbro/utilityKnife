a2e3f05ce5fb49497d69894fbc0e8c0b8b99e024
danelbro <dan.elbro@gmail.com>
2026-05-20 09:17:11 +0100

    Catch errors and allow Termux builds

--- utl_editor/src/editor.cpp
+++ utl_editor/src/editor.cpp
@@ -1,15 +1,22 @@
-#include "EditorApp.hpp"
-
+#include <utl_SDLInterface.hpp>
+#include <utl_Application.hpp>
 #include <SDL3/SDL_main.h>
 
-#include <utl_Application.hpp>
+#include "EditorApp.hpp"
 
-
-int main(int, char **)
+int main(int, char**)
 {
-    utl::AppConfig config{ "UtilityKnife Editor", "0.0.1", "com.utilityKnife.editor",
-                            {1280, 720}, SDL_INIT_VIDEO | SDL_INIT_AUDIO, SDL_WINDOW_RESIZABLE };
-    EditorApp editor{config};
-    editor.run();
+    try {
+        utl::AppConfig config{
+            "UtilityKnife Editor",           "0.0.1",
+            "com.utilityKnife.editor",       {1280, 720},
+            SDL_INIT_VIDEO | SDL_INIT_AUDIO, SDL_WINDOW_RESIZABLE};
+        EditorApp editor{config};
+        editor.run();
+    }
+    catch (utl::SdlException& e) {
+	ERRLOGF("%s\n", e.what());
+        return 1;
+    }
     return 0;
 }
