2c5c78002af0ea16a6a918f8d6a2953fb9a3b7f9
danelbro <dan.elbro@gmail.com>
2026-05-21 18:31:36 +0100

    Mess around with editor title screen

--- utl_editor/include/EditorMain.hpp
+++ utl_editor/include/EditorMain.hpp
@@ -31,5 +31,8 @@ private:
     utl::Box* m_screen{};
     utl::Renderer* m_renderer{};
     std::vector<std::unique_ptr<utl::Entity>> entities{};
+    std::array<utl::Font, 256> fonts{};
+    std::chrono::milliseconds elapsedTime{0};
+    std::chrono::seconds displayTime{0};
 };
 
--- utl_editor/src/EditorMain.cpp
+++ utl_editor/src/EditorMain.cpp
@@ -2,11 +2,11 @@
 
 #include <array>
 #include <chrono>
+#include <cmath>
 #include <filesystem>
 #include <string>
 
 #include "utl_TextObject.hpp"
-#include "utl_Vec2d.hpp"
 #include <utl_Application.hpp>
 #include <utl_SDLInterface.hpp>
 
@@ -15,10 +15,11 @@ EditorMain::EditorMain(utl::Application& app)
       m_renderer{&app.renderer()}
 {
     auto fontPath = std::filesystem::path{"data/Silkscreen.ttf"};
-    auto font = std::make_shared<utl::Font>(utl::createFont(fontPath, 72));
+    fonts.at(0) = utl::Font{utl::createFont(fontPath, 72)};
     auto text = std::make_unique<utl::TextObject>(
-        this, font.get(), utl::Colour{0xff, 0xff, 0xff, 0xff}, "utl_Editor",
-        utl::Vec2d{400.0, 300.0});
+        this, fonts.data(), utl::Colour{0xff, 0xff, 0xff, 0xff},
+        "utl_Editor 0");
+    text->set_pos({0.0, m_screen->w / 2.0});
     entities.emplace_back(std::move(text));
 }
 
@@ -35,9 +36,36 @@ EditorMain::handle_input([[maybe_unused]] std::chrono::milliseconds t,
         return "EditorMain";
 }
 
+static void updateTitle(const std::string& new_text,
+                        std::vector<std::unique_ptr<utl::Entity>>& entities,
+                        utl::Box& screen, std::chrono::milliseconds t)
+{
+    for (auto& entity : entities) {
+        if (!entity)
+            continue;
+        try {
+            auto& to = dynamic_cast<utl::TextObject&>(*entity);
+            to.updateText(new_text);
+            to.set_x_pos(t.count() % screen.w);
+            to.set_y_pos(((1+ std::sin(t.count())) / 2) * screen.h);
+        }
+        catch (std::bad_cast&) {
+            continue;
+        }
+    }
+}
+
 std::string EditorMain::update([[maybe_unused]] std::chrono::milliseconds t,
                                [[maybe_unused]] std::chrono::milliseconds dt)
 {
+    for (auto& entity : entities) {
+        entity->update(t, dt);
+    }
+
+    elapsedTime += dt;
+    displayTime = std::chrono::duration_cast<std::chrono::seconds>(elapsedTime);
+    updateTitle("utl_Editor " + std::to_string(displayTime.count()), entities,
+                *m_screen, t);
     return "EditorMain";
 }
 
